1487e04df404989ba77c81c0c0acbdd49c855e22
Javier Sanz García <pracker@gmail.com>
2023-11-23 12:18:45 +0100

    show hidden files by default in Neo-Tree

--- lua/plugins/pracker.lua
+++ lua/plugins/pracker.lua
@@ -242,7 +242,12 @@ return {
             filesystem = {
                 bind_to_cwd = false,
                 follow_current_file = {enabled = true},
-                use_libuv_file_watcher = true
+                use_libuv_file_watcher = true,
+                filtered_items = {
+                    visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+                    hide_dotfiles = false,
+                    hide_gitignored = true
+                }
             },
             window = {position = "right", mappings = {["<space>"] = "none"}},
             default_component_configs = {
