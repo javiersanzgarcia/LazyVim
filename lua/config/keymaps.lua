b07672f67c47cc0a736f705b4980c69850db9401
root <root@Roshar>
2024-01-02 16:24:31 +0100

    Fis move line motion and typos

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,5 @@
 local keymap = vim.keymap.set
-local silent = {silent = true}
+local silent = { silent = true }
 
 --  Pracker Maps
 keymap("n", "<Leader>q", ":q!<cr>")
@@ -16,70 +16,54 @@ keymap("n", "<Leader>th", ":botright new <Bar> :terminal<cr>")
 -- keymap("n", '<S-right>', '$', silent)
 
 -- Move Lines
-
-keymap("n", '<S-down>', ':m .+1<CR>==gi', silent)
-keymap("n", '<S-up>', ':m .-2<CR>==gi', silent)
-
-keymap("i", '<S-down>', '<Esc>:m .+1<CR>==gi', silent)
-keymap("i", '<S-up>', '<Esc>:m .-2<CR>==gi', silent)
-
-keymap("v", '<S-down>', ":m '>+1<CR>gv=gv", silent)
-keymap("v", '<S-up>', ":m ':m .-2<CR>==gi", silent)
+keymap("v", "<S-down>", ":m '>+1<CR>gv=gv", silent)
+keymap("v", "<S-up>", ":m '<-2<CR>gv=gv", silent)
 
 -- Move around splits
-
-keymap("n", '<A-left>', '<C-w>h', silent)
-keymap("n", '<A-down>', '<C-w>j', silent)
-keymap("n", '<A-up>', '<C-w>k', silent)
-keymap("n", '<A-right>', '<C-w>l', silent)
+keymap("n", "<A-left>", "<C-w>h", silent)
+keymap("n", "<A-down>", "<C-w>j", silent)
+keymap("n", "<A-up>", "<C-w>k", silent)
+keymap("n", "<A-right>", "<C-w>l", silent)
 
 -- Split window
-keymap("n", "ss", ":split<Return>", opts)
-keymap("n", "sv", ":vsplit<Return>", opts)
+keymap("n", "ss", ":split<Return>", silent)
+keymap("n", "sv", ":vsplit<Return>", silent)
 
 -- Format piece
-
-keymap("n", "<leader>fd", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>",
-       silent)
-keymap("v", "<leader>fd", "<cmd>'<.'>lua vim.lsp.buf.range_formatting()<CR>",
-       silent)
+keymap("n", "<leader>fd", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", silent)
+keymap("v", "<leader>fd", "<cmd>'<.'>lua vim.lsp.buf.range_formatting()<CR>", silent)
 
 -- Go to Definition / References / Code Action
 
 keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", silent)
-keymap("n", "gr",
-       "<cmd>lua vim.lsp.buf.references({ includeDeclaration = false })<CR>",
-       silent)
+keymap("n", "gr", "<cmd>lua vim.lsp.buf.references({ includeDeclaration = false })<CR>", silent)
 keymap("n", "ga", "<cmd>lua vim.lsp.buf.code_action()<CR>", silent)
 
 -- Telescope
 
-keymap("n", "<leader>ff",
-       "<CMD>lua require('telescope.builtin').find_files()<CR>")
+keymap("n", "<leader>ff", "<CMD>lua require('telescope.builtin').find_files()<CR>")
 
-keymap("n", "<leader>fg",
-       "<CMD>lua require('telescope.builtin').live_grep()<CR>")
+keymap("n", "<leader>fg", "<CMD>lua require('telescope.builtin').live_grep()<CR>")
 
-keymap("n", "<leader>fc",
-       "<CMD>lua require('telescope.builtin').live_grep({grep_open_files=true})<CR>")
+keymap("n", "<leader>fc", "<CMD>lua require('telescope.builtin').live_grep({grep_open_files=true})<CR>")
 
 keymap("n", "<leader>fb", "<CMD>lua require('telescope.builtin').buffers()<CR>")
 
-keymap("n", "<leader>fn",
-       "<CMD>lua require('telescope.builtin').help_tags()<CR>")
+keymap("n", "<leader>fn", "<CMD>lua require('telescope.builtin').help_tags()<CR>")
 
-keymap("n", "<leader>fw",
-       "<CMD>lua require('telescope.builtin').grep_string()<CR>")
+keymap("n", "<leader>fw", "<CMD>lua require('telescope.builtin').grep_string()<CR>")
 
-keymap("n", "<Leader>pf",
-       "<CMD>lua require('plugins.telescope').project_files({ default_text = vim.fn.expand('<cword>'), initial_mode = 'normal' })<CR>")
+keymap(
+  "n",
+  "<Leader>pf",
+  "<CMD>lua require('plugins.telescope').project_files({ default_text = vim.fn.expand('<cword>'), initial_mode = 'normal' })<CR>"
+)
 
-keymap("n", "<Leader>pw",
-       "<CMD>lua require('telescope.builtin').grep_string({ initial_mode = 'normal' })<CR>")
+keymap("n", "<Leader>pw", "<CMD>lua require('telescope.builtin').grep_string({ initial_mode = 'normal' })<CR>")
 
 -- Buffers
 -- keymap("n", '<Tab>', ':bnext<CR>', silent)
-keymap("n", '<S-Tab>', ':bprev<CR>', silent)
+keymap("n", "<S-Tab>", ":bprev<CR>", silent)
 keymap("n", "gn", ":bn<CR>", silent)
 keymap("n", "gp", ":bp<CR>", silent)
 keymap("n", "<S-q>", ":BufferClose<CR>", silent)
