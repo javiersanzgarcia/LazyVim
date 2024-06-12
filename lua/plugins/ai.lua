a099709974ef2406bc7dc51f36a4118a7e5eabaa
Javier Sanz García <pracker@gmail.com>
2024-06-12 18:03:05 +0200

    Update Supermaven for Codeium (Smae keymaps)

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,20 +1,33 @@
 return {
+	-- {
+	-- 	"Exafunction/codeium.vim",
+	-- 	build = ":Codeium Auth",
+	-- 	config = function()
+	-- 		vim.keymap.set("i", "<C-g>", function()
+	-- 			return vim.fn["codeium#Accept"]()
+	-- 		end, { expr = true })
+	-- 		vim.keymap.set("i", "<C-l>", function()
+	-- 			return vim.fn["codeium#CycleCompletions"](1)
+	-- 		end, { expr = true })
+	-- 		vim.keymap.set("i", "<C-M>", function()
+	-- 			return vim.fn["codeium#Complete"]()
+	-- 		end, { expr = true })
+	-- 		vim.keymap.set("i", "<C-x>", function()
+	-- 			return vim.fn["codeium#Clear"]()
+	-- 		end, { expr = true })
+	-- 	end,
+	-- },
+
 	{
-		"Exafunction/codeium.vim",
-		build = ":Codeium Auth",
+		"supermaven-inc/supermaven-nvim",
 		config = function()
-			vim.keymap.set("i", "<C-g>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true })
-			vim.keymap.set("i", "<C-l>", function()
-				return vim.fn["codeium#CycleCompletions"](1)
-			end, { expr = true })
-			vim.keymap.set("i", "<C-M>", function()
-				return vim.fn["codeium#Complete"]()
-			end, { expr = true })
-			vim.keymap.set("i", "<C-x>", function()
-				return vim.fn["codeium#Clear"]()
-			end, { expr = true })
+			require("supermaven-nvim").setup({
+				keymaps = {
+					accept_suggestion = "<C-g>",
+					clear_suggestion = "<C-x>",
+					accept_word = "<C-j>",
+				},
+			})
 		end,
 	},
 }
