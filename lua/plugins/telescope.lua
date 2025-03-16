e188308aee946db6ad252d31309b78a6335f7b92
javiersanzgarcia <pracker@gmail.com>
2025-03-16 13:32:56 +0100

    wip: remove config of telescope, ui is not changing right now

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,47 +1,12 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
-    "nvim-telescope/telescope-live-grep-args.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local transform_mod = require("telescope.actions.mt").transform_mod
-
-    local trouble = require("trouble")
-    local trouble_telescope = require("trouble.sources.telescope")
-
-    -- or create your custom action
-    local custom_actions = transform_mod({
-      open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
-      end,
-    })
-
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        layout_config = {
-          width = 0.95,
-          height = 0.95,
-        },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            ["<C-t>"] = trouble_telescope.open,
-          },
-        },
-      },
-    })
-
-    telescope.load_extension("fzf")
-    telescope.load_extension("live_grep_args")
-  end,
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-telescope/telescope-live-grep-args.nvim",
+	},
+	config = function()
+		local telescope = require("telescope")
+		telescope.load_extension("fzf")
+		telescope.load_extension("live_grep_args")
+	end,
 }
