ed27a95f33744fde7ebd19157ea1df0af997450f
Javier Sanz García <pracker@gmail.com>
2025-04-12 16:19:23 +0200

    feat: deactivate copilot from cmp menu

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,20 +1,23 @@
 return {
-	"zbirenbaum/copilot.lua",
-	optional = true,
-	opts = function()
-		require("copilot.api").status = require("copilot.status")
-		require("copilot.api").filetypes = {
-			filetypes = {
-				yaml = false,
-				markdown = false,
-				help = false,
-				gitcommit = false,
-				gitrebase = false,
-				hgcommit = false,
-				svn = false,
-				cvs = false,
-				["."] = false,
-			},
-		}
-	end,
+	{
+		"zbirenbaum/copilot.lua",
+		optional = true,
+		opts = function()
+			require("copilot.api").status = require("copilot.status")
+			require("copilot.api").filetypes = {
+				filetypes = {
+					yaml = false,
+					markdown = false,
+					help = false,
+					gitcommit = false,
+					gitrebase = false,
+					hgcommit = false,
+					svn = false,
+					cvs = false,
+					["."] = false,
+				},
+			}
+		end,
+	},
+	{ "giuxtaposition/blink-cmp-copilot", enable = false },
 }
