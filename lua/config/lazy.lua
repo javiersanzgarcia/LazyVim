7ddbd4348e3ee8dfb5ce0c1e5b8ef3105bbeb991
Javier Sanz García <pracker@gmail.com>
2025-04-13 01:56:24 +0200

    feat: added support for go language

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.astro" },
 		-- { import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.git" },
+		{ import = "lazyvim.plugins.extras.lang.go" },
 		{ import = "lazyvim.plugins.extras.lang.java" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.markdown" },
