ba29e7b5c6227bcb2029ec88e191c0011cc0da73
Javier Sanz García <pracker@gmail.com>
2025-04-16 10:49:22 +0200

    feat: Reconfig refactoring plugin as LazyExtra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.editor.mini-files" },
 		{ import = "lazyvim.plugins.extras.editor.harpoon2" },
 		{ import = "lazyvim.plugins.extras.editor.snacks_picker" },
+		{ import = "lazyvim.plugins.extras.editor.refactoring" },
 		{ import = "lazyvim.plugins.extras.formatting.prettier" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
 		{ import = "lazyvim.plugins.extras.lang.angular" },
